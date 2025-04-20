ad7d33ee6597669272a4d459a37969991628428c
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2025-04-21 01:47:28 +0530

    Add main function to run Streamlit app with environment setup
    - Introduced a main function in run.py to launch the Streamlit application.
    - Configured environment variables for TensorFlow and PyTorch to suppress warnings.
    - Set up the event loop for Windows compatibility.
    - Streamlined the execution of the Streamlit app by dynamically determining the app path.

--- src/run.py
+++ src/run.py
@@ -1,1 +1,31 @@
- 
+import asyncio
+import streamlit.web.bootstrap
+import streamlit.web.cli as stcli
+import sys
+import os
+import warnings
+import logging
+
+# Suppress warnings
+warnings.filterwarnings('ignore')
+logging.getLogger('tensorflow').setLevel(logging.ERROR)
+logging.getLogger('torch').setLevel(logging.ERROR)
+
+def main():
+    # Get the absolute path to the app.py file
+    app_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "app.py")
+    
+    # Set up the event loop
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    
+    # Set environment variables
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    os.environ['PYTORCH_WARN_ONCE'] = '0'
+    
+    # Run Streamlit
+    sys.argv = ["streamlit", "run", app_path]
+    sys.exit(stcli.main())
+
+if __name__ == "__main__":
+    main() 
